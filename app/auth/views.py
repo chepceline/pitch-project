a18bcde7a27a6f6a8dc50965e26e7f41ede07322
chepceline <chepceline25@gmail.com>
2022-05-18 13:46:39 +0300

    update password

--- app/auth/views.py
+++ app/auth/views.py
@@ -36,7 +36,7 @@ def login():
     login_form =LoginForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(username = login_form.username.data).first()
-        if user is not None and user.password_secure(login_form.password.data):
+        if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password')
