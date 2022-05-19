1ae8692e5a40d273af3ca0380e8d35b71879efdd
chepceline <chepceline25@gmail.com>
2022-05-19 11:24:59 +0300

    update config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 class Config:
     
-    SECRET_KEY= os.environ.get('SECRET KEY')
+    SECRET_KEY='aa;ag8CFuGqen;YpA}}-G%64C}ggiN'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER ='smtp.googlemail.com'
     MAIL_PORT =587
@@ -13,10 +13,12 @@ class Config:
 
 class ProdConfig(Config):
     
-
-    pass
-
+    uri = os.getenv('DATABASE_URL')
+    if uri and uri.startswith('postgres://'):
+        uri = uri.replace('postgres://', 'postgresql://', 1)
+    SQLALCHEMY_DATABASE_URI = uri
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://damaris:1234@localhost/power'
     DEBUG = True
     
 
